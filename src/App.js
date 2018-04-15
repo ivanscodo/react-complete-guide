3a2bc3a753bc301095d0876df4c15752670018d6
Ivan Sotelo <ivansotello@yahoo.com.br>
2018-04-15 23:57:58 +0200

    two way binding

--- src/App.js
+++ src/App.js
@@ -4,17 +4,56 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  state = {
+    persons: [{
+      "name": "Teste",
+      "age": 27
+    },
+    {
+      "name": "Ivan1",
+      "age": 28
+    },
+    {
+      "name":"Ivan2",
+      "age": 29
+    }
+    ]
+  }
+
+  nameChangedHandler = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      persons: [
+      {
+        "name": event.target.value,
+        "age": 27
+      },
+      {
+        "name": "Ivan1",
+        "age": 28
+      },
+      {
+        "name":"Ivan2",
+        "age": 29
+      }
+      ]});
+      console.log(this.state.persons[0].name);
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Person />
+      <div>
+        <Person changed={this.nameChangedHandler} name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
+
 }
 
 export default App;
