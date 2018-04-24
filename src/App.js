86355f67a135db5846e450df23cb05c997cf2d96
icodo <icodo@gmail.com>
2018-04-24 11:38:43 +0200

    implementing lists

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-  }
 
   state = {
-    persons: [{
-      "name": "Teste",
-      "age": 27
-    },
-    {
-      "name": "Ivan1",
-      "age": 28
-    },
-    {
-      "name":"Ivan2",
-      "age": 29
-    }
-    ],
+    persons: [
+      {
+        id: 'aasdadsdasdas',
+        name: 'Ivan0',
+        age: 27
+      },
+      {
+        id: 'asdasdasdasds',
+        name: 'Ivan1',
+        age: 28
+      },
+      {
+        id: 'aaaaaaaaaasssdsdsds',
+        name: 'Ivan2',
+        age: 29
+      }
+      ],
     showPersons: false,
   }
+
+  
 
   hidePersons = () => {
     const show = this.state.showPersons;
@@ -31,40 +33,63 @@ class App extends Component {
   }
 
   nameChangedHandler = (event) => {
+    const personId = event.target.getAttribute("data-index");
+    
+    let personsArray = [...this.state.persons];
+    let currentPersonPosition = personsArray.findIndex(person => {
+      return person.id === personId;
+    });
+
+    let currentPerson = personsArray[currentPersonPosition];
+    currentPerson.name = event.target.value;
+
     this.setState({
-      persons: [
-      {
-        "name": event.target.value,
-        "age": 27
-      },
-      {
-        "name": "Ivan1",
-        "age": 28
-      },
-      {
-        "name":"Ivan2",
-        "age": 29
-      }
-      ]});
-    }
+      persons: personsArray
+    });
+  }
+  
+  deletePersonHandler = (event) => {
+    let position = event.target.getAttribute("data-index");
+    let personsArray = [...this.state.persons];
+    personsArray.splice(position, 1);
+    
+    this.setState({
+      persons: personsArray
+    });
+
+  }
 
   render() {
+    console.log("App render");
+    let persons = null;
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          {
+            this.state.persons.map((person, index) => {
+              return (
+                    <Person
+                        click={this.deletePersonHandler}
+                        name={person.name} 
+                        age={person.age} 
+                        changed={this.nameChangedHandler}
+                        index={person.id}
+                        key={person.id}
+                    />
+              );
+          })
+        }
+        </div>
+      )
+    }
+
     return (
       <div>
         <button onClick={this.hidePersons}>Show/hide persons</button>
-        { this.state.showPersons ? 
-          <div>
-            <Person changed={this.nameChangedHandler} name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-          </div>
-          :
-          null
-        }
+          {persons}
       </div>
     );
   }
-
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,15 +3,20 @@ import './Person.css';
 
 
 class Person extends Component{
-  
+
+  constructor(props){
+    super(props);
+  }
+
   render(){
     return(
-      <div className="Person">
-        <p>Name: {this.props.name}</p>
+      <div className="Person" >
+        <p onClick={this.props.click} data-index={this.props.index}>Name: {this.props.name}</p>
         <p>Age: {this.props.age}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+        <input type="text" onChange={this.props.changed} value={this.props.name} data-index={this.props.index}/>
       </div>
     );
   }
 }
+
 export default Person;
